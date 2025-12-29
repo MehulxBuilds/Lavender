31bdc694e96eae7f0f3245ea41c00556a83cd1cf
Mehul Prajapati <ailearn90@gmail.com>
2025-12-29 19:03:46 +0530

    added authentication using better-auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+import LogoutButton from '@/components/layout/logout-button';
 import { Button } from '@/components/ui/button'
-import React from 'react'
 
 const page = () => {
   return (
-    <Button>
-      Hello
-    </Button>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <LogoutButton>
+        <Button>Logout</Button>
+      </LogoutButton>
+    </div>
   )
 }
 
-export default page
+export default page;
---
+++ src/utils/auth-utils.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation"
+
+export const requireAuth = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
+    return session;
+};
+
+export const requireUnAuth = async () => {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    if (session) {
+        redirect("/");
+    }
+
+    return session;
+};
