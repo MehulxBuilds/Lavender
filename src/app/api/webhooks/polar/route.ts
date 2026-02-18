f4315a0bb7cd9cab15844de52123782269d7ee70
Mehul Prajapati <ailearn90@gmail.com>
2026-02-19 01:11:24 +0530

    feat: payments through polar.sh

---
+++ src/app/api/webhooks/polar/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    return NextResponse.json({
+        received: true,
+    });
+
+    
+}
